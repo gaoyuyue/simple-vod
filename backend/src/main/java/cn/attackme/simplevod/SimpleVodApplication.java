43f0a26e49219746f685682af6523cf368b75af4
高语越 <gaoyuyue@outlook.com>
2018-06-28 14:44:30 +0800

    Initial backend project

---
+++ backend/src/main/java/cn/attackme/simplevod/SimpleVodApplication.java
@@ -0,0 +1,12 @@
+package cn.attackme.simplevod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleVodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleVodApplication.class, args);
+    }
+}
